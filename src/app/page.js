eeb2ce53420b03f2dbbf4e823744a13420d0bc9f
Samuel <samuelajala01@gmail.com>
2024-05-11 11:36:45 -0700

    chore: added hero img, edited position

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,15 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="mx-[4vw]">
-        <h1 className="text-5xl mt-[30vh] mb-4 text-white">
-          Pushing Machines to <span className="text-blue-400">Brilliance</span>
+     <section className="mx-[4vw] flex">
+     <div className="flex-[1.5] mt-[25vh]">
+        <h1 className="text-[5vw]  mb-4 text-white">
+          Pushing Machines to <span className="text-blue-400">Brilliance</span>🚀
         </h1>
-        <p className="text-white text-2xl md:w-[40vw]">Great things from a small place, pushing codes that will change the world, one line at a time.</p>
+        <p className="text-white md:text-2xl md:w-[40vw]">Great things from a small place, pushing codes that will change the world, one line at a time.</p>
       </div>
+      <div className=" flex flex-1 text-[20vw] p-0 justify-center ml-16 top-[30rem]">🐱‍👤</div>
+     </section>
     </>
   );
 }
