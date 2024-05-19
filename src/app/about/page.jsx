789cc59341337d44a6286fe23405ea77301b0cb0
Samuel <samuelajala01@gmail.com>
2024-05-19 06:48:19 -0700

    feat: changed pry color, added rick img

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,6 @@
 import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import profileImg from "../../../public/rick-pic.jpg"
 
 const About = () => {
   return (
@@ -6,7 +8,8 @@ const About = () => {
       <Navbar />
       <section className="mx-[4vw]">
         <h2 className="text-4xl md:text-6xl my-4">Meet me</h2>
-        <p className="text-[1.1rem] md:text-[1.4rem] opacity-50">In case you didn't catch that, I'm Samuel Ajala, currently in my third year studying Electronics and Computer Engineering at the Lagos State University.</p>
+       <div className="block md:flex"><p className="text-[1.1rem] md:text-[1.4rem] opacity-75 flex-[1.5]">In case you didn't catch that, I'm Samuel Ajala, currently in my third year studying Electronics and Computer Engineering at the Lagos State University. I have some experience in Frontend development, Additive Manufacturing, Internet of Things, and Embedded systems</p><div className="hidden md:flex flex-[1] items-center justify-center"><Image src={profileImg} className="border-8 rounded-lg border-dotted w-3/4"/></div></div>
+        
       </section>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
-// import rick from "../../public/rick-pic.jpg";
 import Link from "next/link"
+import Image from "next/image";
+import rick from "../../public/rick-pic.jpg"
 
 const Navbar = () => {
     return (
       <>
         <nav className="flex justify-between py-4 px-[4vw] items-center">
             <div className="flex">
-            {/* <div><img src={rick} alt="Image of rick" width={50}/></div> */}
-                <div><p className="text-[#7192E6]">Samuel Ajala</p>
+            <div className="pr-2"><Image src={rick} alt="Image of rick" width={50} className="rounded-full"/></div>
+                <div><p className="text-[var(--pry)] font-semibold">Samuel Ajala</p>
                 <p className="text-white">Software Developer(ML)</p></div>
                 
             </div>
@@ -25,7 +26,7 @@ const Navbar = () => {
               <Link href="" className="text-white font-medium">Reach me</Link>
             </li>
           </ul>
-          <div className="flex py-1 px-[2vw] bg-[#7192e6] rounded">Location: UK</div>
+          <div className="flex py-1 px-[2vw] bg-[var(--pry)] rounded">Location: UK</div>
         </nav>
         
       </>
