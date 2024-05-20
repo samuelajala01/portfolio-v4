c4b0681a523ece08381686ac7858b3c4f3e28c9c
Samuel <samuelajala01@gmail.com>
2024-05-20 13:30:52 -0700

    chore: changed manual project list to json

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,19 +1,48 @@
 import Navbar from "@/components/Navbar";
 import Project from "@/components/Project";
 
+const projectList = [
+  {
+    title: "Object Classifier using CNN",
+    desc: "In case you didn't catch that, I'm Samuel Ajala, currently in my third year studying Electronics and Computer Engineering at the Lagos State University. I have some experience in Frontend development, Additive Manufacturing, Internet of Things, and Embedded systems",
+    type: "solo",
+  },
+  {
+    title: "Language Identification Model",
+    desc: "In case you didn't catch that, I'm Samuel Ajala, currently in my third year studying Electronics and Computer Engineering at the Lagos State University. I have some experience in Frontend development, Additive Manufacturing, Internet of Things, and Embedded systems",
+    type: "solo",
+  },{
+    title: "SMS Spam Classifier",
+    desc: "In case you didn't catch that, I'm Samuel Ajala, currently in my third year studying Electronics and Computer Engineering at the Lagos State University. I have some experience in Frontend development, Additive Manufacturing, Internet of Things, and Embedded systems",
+    type: "team",
+  },
+];
+
 const projects = () => {
   return (
-    <><Navbar />
+    <>
+      <Navbar />
+      
       <section className="mx-[4vw] mt-8 text-center mb-32">
-      <h1 className="text-2xl md:text-[4vw] text-center mb-16">See my projects</h1>
-     <div className="m-auto"> <input type="search" className="py-2 pl-[1rem] w-full text-[var(--pry)] bg-[#090f14] mb-4"/>
-      <button className="py-2 px-4 bg-blue-600 w-full block font-semibold">Search</button></div></section>
+        <h1 className="text-2xl md:text-[4vw] text-center mb-16">
+          See my projects
+        </h1>
+        <div className="m-auto">
+          {" "}
+          <input
+            type="search"
+            className="py-2 pl-[1rem] w-full text-[var(--pry)] bg-[#090f14] mb-4 text-2xl"
+          />
+          <button className="py-2 px-4 bg-blue-600 w-full block font-semibold">
+            Search
+          </button>
+        </div>
+      </section>
 
       <div className="mx-[4vw]">
-        <Project title="Project 1" desc="In case you didn't catch that, I'm Samuel Ajala, currently in my third year studying Electronics and Computer Engineering at the Lagos State University. I have some experience in Frontend development, Additive Manufacturing, Internet of Things, and Embedded systems" type="solo"/>
-        <Project title="Project 2" desc="In case you didn't catch that, I'm Samuel Ajala, currently in my third year studying Electronics and Computer Engineering at the Lagos State University. I have some experience in Frontend development, Additive Manufacturing, Internet of Things, and Embedded systems" type="solo"/>
-        <Project title="Project 3" desc="In case you didn't catch that, I'm Samuel Ajala, currently in my third year studying Electronics and Computer Engineering at the Lagos State University. I have some experience in Frontend development, Additive Manufacturing, Internet of Things, and Embedded systems" type="solo"/>
-        <Project title="Project 4" desc="In case you didn't catch that, I'm Samuel Ajala, currently in my third year studying Electronics and Computer Engineering at the Lagos State University. I have some experience in Frontend development, Additive Manufacturing, Internet of Things, and Embedded systems" type="solo"/>
+      {projectList.map((item) => (
+        <Project key={item.title} title={item.title} desc={item.desc} type={item.type} />
+      ))}
       </div>
     </>
   );
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 const Project = ({ title, type, desc }) => {
   return (
     <>
-      <div className="max-w-[64rem] mx-auto bg-black bg-opacity-[0.7] rounded-md p-6 text-left mb-16">
+      <div className="max-w-[64rem] mx-auto bg-black bg-opacity-[0.7] rounded-md p-8 text-left mb-16">
         <h2 className="text-2xl md:text-4xl ">
           <Link href="./">{title}</Link>
         </h2>
-        <p className="my-4">{desc}</p>
-        <p className="">{type}</p>
+        <p className="mt-4 mb-8">{desc}</p>
+        <p className="bg-gray-700 w-fit rounded p-[0.1rem]">{type}</p>
       </div>
     </>
   );
