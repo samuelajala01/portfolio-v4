5c0c944dd284db2dbc5ac2dbc5320d4ab1ae80b1
Samuel Ajala <samuelajala01@gmail.com>
2024-05-21 13:56:05 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
      <section className="mx-[6vw] lg::mx-[4vw] flex">
      <div className="flex-[1.5] md:mt-[25vh]">
         <h1 className="text-[5vw]  mb-4 text-white">
-          Pushing Machines to <span className="text-[var(--pry)]">Brilliance</span>🚀
+          Pushing Machines to <span className="text-[var(--pry)]">Brilliance</span> &#128640;
         </h1>
         <p className="text-white md:text-2xl md:w-[40vw]">Great things from a small place, pushing codes that will change the world, one line at a time.</p>
       </div>
