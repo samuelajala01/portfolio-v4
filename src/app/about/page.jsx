7fc77465b69763e0465027b34baf614f2f19ef91
Samuel <samuelajala01@gmail.com>
2024-05-25 14:33:10 -0700

    chore: fix build error

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <Navbar />
       <section className="mx-[4vw]">
         <h2 className="text-4xl md:text-6xl my-4">Meet me</h2>
-       <div className="block md:flex"><p className="text-[1.1rem] md:text-[1.4rem] opacity-75 flex-[1.5]">In case you didn't catch that, I'm Samuel Ajala, currently in my third year studying Electronics and Computer Engineering at the Lagos State University. I have some experience in Frontend development, Additive Manufacturing, Internet of Things, and Embedded systems</p><div className="hidden md:flex flex-[1] items-center justify-center"><Image src={profileImg} alt="profile image" className="border-8 rounded-lg border-dotted w-3/4"/></div></div>
+       <div className="block md:flex"><p className="text-[1.1rem] md:text-[1.4rem] opacity-75 flex-[1.5]">In case you didn&#39;t catch that, I&#39;m Samuel Ajala, currently in my third year studying Electronics and Computer Engineering at the Lagos State University. I have some experience in Frontend development, Additive Manufacturing, Internet of Things, and Embedded systems</p><div className="hidden md:flex flex-[1] items-center justify-center"><Image src={profileImg} alt="profile image" className="border-8 rounded-lg border-dotted w-3/4"/></div></div>
         
       </section>
     </>
--- src/app/page.js
+++ src/app/page.js
@@ -8,11 +8,11 @@ export default function Home() {
      <section className="mx-[6vw] lg::mx-[4vw] flex">
      <div className="flex-[1.5] md:mt-[25vh]">
         <h1 className="text-[5vw]  mb-4 text-white">
-          Pushing Machines to <span className="text-[var(--pry)]">Brilliance</span> {"&#128640;"}
+          Pushing Machines to <span className="text-[var(--pry)]">Brilliance</span> 
         </h1>
         <p className="text-white md:text-2xl md:w-[40vw]">Great things from a small place, pushing codes that will change the world, one line at a time.</p>
       </div>
-      <div className=" flex flex-1 md:text-[20vw] p-0 justify-center ml-16  md:top-[20rem] mt-2 lg:mt-8 hidden md:block"> {"&#129399;"} </div>
+      <div className=" flex flex-1 md:text-[20vw] p-0 justify-center ml-16  md:top-[20rem] mt-2 lg:mt-8 hidden md:block"> 🐱‍👤</div>
      </section>
     </>
   );
