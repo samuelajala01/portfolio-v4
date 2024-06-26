f10e3fc9cee02be261d4993f023d34bd64150e84
Samuel <samuelajala01@gmail.com>
2024-06-26 13:17:47 +0100

    chore: changed profile pic

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
-import profileImg from "../../public/rick-pic.jpg";
+import profileImg from "../../public/my-pic.jpg";
 
 export default function Home() {
   return (
