bc64991ae013bc90e87a9776827b6bb202fa1eb9
Samuel <samuelajala01@gmail.com>
2024-07-14 21:45:05 +0100

    style: fix social icons responsiveness

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,9 @@
 import SocialBox from "@/components/SocialBox";
-
 import linkedin from "../../../public/rick-pic.jpg"
 
 const Contact = () => {
     return ( <section className="mx-[6vw] lg::mx-[4vw] text-center">
-       <h1>Reach Out</h1>
+       <h1 className="text-4xl my-8">Reach Out</h1>
        <div className="block sm:flex sm:justify-around">
         <SocialBox name="LinkedIn" link="/" img_link={linkedin}/>
         <SocialBox name="Twitter" link="/" img_link={linkedin}/>
--- src/components/SocialBox.jsx
+++ src/components/SocialBox.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 const SocialBox = (props) => {
   return (
     <>
-      <div className="border-blue-600 rounded-md text-center p-8 bg-black bg-opacity-[0.5]">
-        <Image src={props.img_link} className="w-[5rem] mb-8 m-auto" />
+     <Link href={props.link}> <div className="border-blue-600 rounded-md text-center p-8 bg-black bg-opacity-[0.5] w-[10rem] mb-8 m-auto">
+        <Image src={props.img_link} className="w-[5rem] mb-8 m-auto" alt="Image of a social media icon"/>
         <p>{props.name}</p>
-      </div>
+      </div></Link>
     </>
   );
 };
