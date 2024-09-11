21c457f7cc32381884797e5e5b13fe42fb66d4cf
Samuel Ajala <samuelajala01@gmail.com>
2024-09-11 23:03:58 +0000

    style: made about page content responsive

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <>
       <section className="mx-[4vw]">
         <h2 className="text-4xl md:text-6xl my-8 font-bold text-[var(--pry)]">Get to know me</h2>
-        <div className="block md:flex items-center">
+        <div className="inline-flex flex flex-col-reverse sm:flex-row items-center">
           <p className="text-[1.1rem] md:text-[1.4rem] opacity-75 flex-[1.5]">
             In case you didn&#39;t catch that, I&#39;m Samuel Ajala, currently
             in my third year studying Electronics and Computer Engineering at
@@ -14,7 +14,7 @@ const About = () => {
             development, Additive Manufacturing, Internet of Things, and
             Embedded systems.
           </p>
-          <div className="hidden md:flex flex-[1] items-center justify-center">
+          <div className=" flex justify-center md:flex flex-[1] mb-8 sm:mb-0">
             <Image
               src={profileImg}
               alt="profile image"
