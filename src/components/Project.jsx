96ce8f61482dce1ea50d8e43f56c22bb4361fe25
Samuel <samuelajala01@gmail.com>
2024-09-18 15:36:41 -0700

    fix: add tools to project card

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,15 +1,31 @@
 import Link from "next/link";
 
-const Project = ({ title, type, desc, tags }) => {
+const Project = ({ title, type, desc, tags, tools }) => {
   return (
     <>
       <div className="max-w-[64rem] mx-auto bg-black bg-opacity-[0.5] rounded-md p-8 px-12 text-left mb-16 border-1 border-black">
         <h2 className="text-2xl md:text-4xl font-semibold hover:text-blue-600">
           <Link href="./">{title}</Link>
         </h2>
+        
+     
         <p className="mt-10 mb-4">{desc}</p>
+        
+
         <p className="mt-10 mb-8">{tags}</p>
+
         <p className="bg-gray-700 w-fit rounded p-[0.1rem]">{type}</p>
+
+        <div className="flex flex-wrap gap-2 mt-6">
+          {tools.map((tool, index) => (
+            <span
+              key={index}
+              className="bg-gray-900  p-[0.3rem] rounded-sm text-sm"
+            >
+              {tool}
+            </span>
+          ))}
+        </div>
       </div>
     </>
   );
