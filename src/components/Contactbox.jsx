f8850adf8bc333e3740367b949654c53b2a49323
Samuel <samuelajala01@gmail.com>
2024-09-29 14:23:30 +0100

    style: remove contactbox hover

--- src/components/Contactbox.jsx
+++ src/components/Contactbox.jsx
@@ -5,13 +5,13 @@ const ContactBox = () => {
     return (
         <div className="ml-[5vw] w-fit mb-16 sm:mb-0 left-[5vw] p-4 bg-black bg-opacity-[0.5] rounded-sm flex space-x-4">
             <a href="https://www.twitter.com/samuelajala01" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-                <FaTwitter className="text-blue-400 hover:text-blue-600" size={32} />
+                <FaTwitter className="text-blue-600" size={32} />
             </a>
             <a href="https://www.linkedin.com/in/samuelajala01" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
-                <FaLinkedin className="text-blue-700 hover:text-blue-900" size={32} />
+                <FaLinkedin className="text-blue-700" size={32} />
             </a>
             <a href="https://www.github.com/samuelajala01" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
-                <FaGithub className="text-gray-800 hover:text-gray-900" size={32} />
+                <FaGithub className="text-white-800" size={32} />
             </a>
             
         </div>
