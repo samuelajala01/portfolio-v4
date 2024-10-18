95030a57991c87db39ff168b3f417618461a30ee
Samuel <samuelajala01@gmail.com>
2024-10-18 23:30:10 +0100

    chore: add keywords in meta for SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,9 @@ import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Samuel Ajala - Software Developer (ML)",
-  description: "Embedded Sytems Engineer || Software Developer",
+  title: "Samuel Ajala - Machine Learning Engineer & Software Developer",
+  description: "Expert in Embedded Systems, Machine Learning, AI, Robotics, and Full-Stack Development. Crafting intelligent software solutions for automation and innovation.",
+  keywords: "Samuel Ajala, Ajala, Software Developer, Machine Learning Engineer, Embedded Systems, AI, Robotics, Full-Stack Development, Automation, Artificial Intelligence, Developer Portfolio, ML, ML Engineer, Electronics and Computer Engineering, Python, Computer Vision "
 };
 
 export default function RootLayout({ children }) {
--- src/app/page.js
+++ src/app/page.js
@@ -10,25 +10,25 @@ import { FaRocket } from 'react-icons/fa';
 
 export default function Home() {
 
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const handleLoad = () => {
-      setLoading(false);
-    };
+  // useEffect(() => {
+  //   const handleLoad = () => {
+  //     setLoading(false);
+  //   };
 
-    if (document.readyState === 'complete') {
-      handleLoad();
-    } else {
-      window.addEventListener('load', handleLoad);
-    }
+  //   if (document.readyState === 'complete') {
+  //     handleLoad();
+  //   } else {
+  //     window.addEventListener('load', handleLoad);
+  //   }
 
-    return () => window.removeEventListener('load', handleLoad);
-  }, []);
+  //   return () => window.removeEventListener('load', handleLoad);
+  // }, []);
 
-  if (loading) {
-    return <Loader />;
-  }
+  // if (loading) {
+  //   return <Loader />;
+  // }
 
   return (
     <>
