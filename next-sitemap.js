e8a429d5ff68d793875918151d1fcf9752d73d3c
Samuel <samuelajala01@gmail.com>
2024-11-17 18:26:31 +0100

    feat: add shadow to profile img

---
+++ next-sitemap.js
@@ -0,0 +1,9 @@
+// next-sitemap.js
+module.exports = {
+    siteUrl: 'https://cy63rx.vercel.app', // Replace with your actual domain
+    generateRobotsTxt: true, // (optional) Generate robots.txt file
+    changefreq: 'daily', // (optional) Default change frequency
+    priority: 0.7, // (optional) Default priority for pages
+    sitemapSize: 5000, // (optional) Maximum number of URLs per sitemap
+  };
+  
--- src/app/page.js
+++ src/app/page.js
@@ -47,11 +47,11 @@ export default function Home() {
             </p>
           </div>
           <div className="flex flex-1 justify-around">
-          <div>
+          <div className='rounded-full h-[15rem] w-[15rem] shadow-[45px_15px_5px_rgba(0,0,0,0.3)]'>
             <Image
               src={profileImg}
               alt="profile image"
-             className="rounded-full border-blue-600 h-[15rem] w-[15rem]"
+             className="rounded-full"
             />
           </div>
         </div>
