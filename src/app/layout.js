6c490000606362368c1f3676e55471c0de217dc3
Samuel <samuelajala01@gmail.com>
2024-12-19 23:55:27 +0100

    feat: add OpenGraph and Twitter metadata to improve SEO and social sharing

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,52 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Samuel Ajala - Machine Learning Engineer & Software Developer",
   description: "Expert in Embedded Systems, Machine Learning, AI, Robotics, and Full-Stack Development. Crafting intelligent software solutions for automation and innovation.",
-  keywords: "Samuel Ajala, Ajala, Software Developer, Machine Learning Engineer, Embedded Systems, AI, Robotics, Full-Stack Development, Automation, Artificial Intelligence, Developer Portfolio, ML, ML Engineer, Electronics and Computer Engineering, Python, Computer Vision "
+  keywords: "Samuel Ajala, Ajala, Software Developer, Machine Learning Engineer, Embedded Systems, AI, Robotics, Full-Stack Development, Automation, Artificial Intelligence, Developer Portfolio, ML, ML Engineer, Electronics and Computer Engineering, Python, Computer Vision",
+  
+  // OpenGraph metadata
+  openGraph: {
+    title: "Samuel Ajala - Machine Learning Engineer & Software Developer",
+    description: "Expert in Embedded Systems, Machine Learning, AI, Robotics, and Full-Stack Development. Crafting intelligent software solutions for automation and innovation.",
+    url: "https://cy63rx.vercel.app",
+    siteName: "Samuel Ajala Portfolio",
+    images: [
+      {
+        url: "/public/avatar.png", 
+        width: 1200,
+        height: 630,
+        alt: "Samuel Ajala - Software Developer (ML)",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  // Twitter metadata
+  twitter: {
+    card: "summary_large_image",
+    title: "Samuel Ajala - Machine Learning Engineer & Software Developer",
+    description: "Expert in Embedded Systems, Machine Learning, AI, Robotics, and Full-Stack Development. Crafting intelligent software solutions for automation and innovation.",
+    creator: "@samuelajala01", 
+    images: ["/public/avatar.png"],
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  verification: {
+    google: "your-google-verification-code",
+  },
+  alternates: {
+    canonical: "https://cy63rx.vercel.app",
+  },
 };
 
 export default function RootLayout({ children }) {
